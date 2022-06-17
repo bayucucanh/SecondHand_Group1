c1e3ae04aad66d130a5f048a0eb2b08a5b9b675c
Yobel <42953586+yobel47@users.noreply.github.com>
2022-06-17 23:21:14 +0700

    Update index.js

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,7 +1,7 @@
 import Login from './auth/Login';
 import Register from './auth/Register';
-import Home from './home';
-import Splash from './splash';
+import Home from './Home';
+import Splash from './Splash';
 import Profile from './Profile';
 import ChangeProfile from './ChangeProfile';
 
