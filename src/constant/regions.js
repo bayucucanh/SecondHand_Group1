10d0506fd8c06ee4d039ee2a60bf1c56abb082e3
Yobel <vega.yobel.2@gmail.com>
2022-06-17 23:57:39 +0700

    Fix major issue

--- src/constant/regions.js
+++ src/constant/regions.js
@@ -515,15 +515,4 @@ export const regions = [
   { label: 'Kab. Sorong', value: 'Kab. Sorong' },
 ];
 
-export default regions.sort((a, b) => {
-  const nameA = a.label.toUpperCase();
-  const nameB = b.label.toUpperCase();
-  if (nameA < nameB) {
-    return -1;
-  }
-  if (nameA > nameB) {
-    return 1;
-  }
-
-  return 0;
-});
+export default regions;
