e4a1fac38587f7c45e94aa0655cebe49e1bc7ed7
bayucucanh <58686350+bayucucanh@users.noreply.github.com>
2022-06-30 21:14:33 +0700

    Update Detail

--- src/components/BottomSheet/index.js
+++ src/components/BottomSheet/index.js
@@ -9,6 +9,7 @@ import { Formik } from 'formik';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigation } from '@react-navigation/native';
+import { ScrollView } from 'react-native-gesture-handler';
 import CustomButton from '../CustomButton';
 import InputText from '../InputText';
 import HelperText from '../HelperText';
@@ -19,16 +20,21 @@ import formatRupiah from '../../utils/formatCurrency';
 import { bidProduct } from '../../redux/actions';
 
 function BottomSheetComponent({
-  productName, price, sheetRef, productId, title, placeholder, imageUrl, accessToken, statusBid, navigation,
+  productName,
+  price,
+  sheetRef,
+  productId,
+  title,
+  placeholder,
+  imageUrl,
+  accessToken,
+  navigation,
 }) {
   const { t, i18n } = useTranslation();
   const dispatch = useDispatch();
-  const [status, setStatus] = useState('');
-
-  const allBidProduct = useSelector((state) => state.allBid.allBidProduct);
 
   // variables
-  const snapPoints = useMemo(() => ['1%', '1%', '70%'], []);
+  const snapPoints = useMemo(() => ['1%', '1%', '65%'], []);
 
   // callbacks
   const handleSheetChanges = useCallback((index) => {
@@ -36,12 +42,6 @@ function BottomSheetComponent({
   }, []);
 
   const handleClosePress = () => sheetRef.current.close();
-
-  useEffect(() => {
-    setStatus(filterBid[0]?.status);
-  });
-
-  const filterBid = allBidProduct.filter((item) => item.product_id === productId);
 
   const submitBid = (bid) => {
     const data = {
@@ -90,78 +90,80 @@ function BottomSheetComponent({
           errors,
           isValid,
         }) => (
-          <View
-            style={{
-              flex: 1,
-              backgroundColor: COLORS.white,
-              padding: SIZES.h2,
-            }}
-          >
-            <Text style={{ color: COLORS.black, ...FONTS.bodyNormalMedium }}>
-              Masukan Harga Tawarmu
-            </Text>
-            <Text style={{ color: COLORS.neutral3, ...FONTS.bodyNormalMedium }}>
-              Harga tawaranmu akan diketahui penjual, jika penjual cocok kamu
-              akan segera dihubungi penjual.
-            </Text>
+          <ScrollView>
             <View
-              style={[
-                styles.card,
-                {
-                  marginTop: SIZES.padding3,
-                  paddingHorizontal: SIZES.padding5,
-                  paddingVertical: SIZES.padding3,
-                  flexDirection: 'row',
-                },
-              ]}
+              style={{
+                flex: 1,
+                backgroundColor: COLORS.white,
+                padding: SIZES.h2,
+              }}
             >
-              <View style={{ justifyContent: 'center' }}>
-                <Image
-                  source={{
-                    uri: imageUrl,
-                  }}
-                  style={{ width: 48, height: 48 }}
+              <Text style={{ color: COLORS.black, ...FONTS.bodyNormalMedium }}>
+                Masukan Harga Tawarmu
+              </Text>
+              <Text style={{ color: COLORS.neutral3, ...FONTS.bodyNormalMedium }}>
+                Harga tawaranmu akan diketahui penjual, jika penjual cocok kamu
+                akan segera dihubungi penjual.
+              </Text>
+              <View
+                style={[
+                  styles.card,
+                  {
+                    marginTop: SIZES.padding3,
+                    paddingHorizontal: SIZES.padding5,
+                    paddingVertical: SIZES.padding3,
+                    flexDirection: 'row',
+                  },
+                ]}
+              >
+                <View style={{ justifyContent: 'center' }}>
+                  <Image
+                    source={{
+                      uri: imageUrl,
+                    }}
+                    style={{ width: 48, height: 48 }}
+                  />
+                </View>
+                <View style={{ paddingLeft: SIZES.padding3 }}>
+                  <Text
+                    style={{ ...FONTS.bodyLargeMedium, color: COLORS.neutral5 }}
+                  >
+                    {productName}
+                  </Text>
+                  <Text
+                    style={{
+                      ...FONTS.bodyNormalRegular,
+                      color: COLORS.neutral3,
+                    }}
+                  >
+                    {formatRupiah(price)}
+                  </Text>
+                </View>
+              </View>
+              <View style={{ marginVertical: SIZES.h2 }}>
+                <Text style={{ ...FONTS.bodyNormalBold }}>{title}</Text>
+                <InputText
+                  placeholder={placeholder}
+                  name="bid_price"
+                  style={{ marginTop: 4 }}
+                  onChangeText={handleChange('bid_price')}
+                  onBlur={handleBlur('bid_price')}
+                  error={touched.bid_price && errors.bid_price}
+                  value={values.bid_price}
+                  type="number-pad"
                 />
+                {touched.bid_price && errors.bid_price && (
+                  <HelperText text={t(errors.bid_price)} />
+                )}
               </View>
-              <View style={{ paddingLeft: SIZES.padding3 }}>
-                <Text
-                  style={{ ...FONTS.bodyLargeMedium, color: COLORS.neutral5 }}
-                >
-                  {productName}
-                </Text>
-                <Text
-                  style={{
-                    ...FONTS.bodyNormalRegular,
-                    color: COLORS.neutral3,
-                  }}
-                >
-                  {formatRupiah(price)}
-                </Text>
-              </View>
-            </View>
-            <View style={{ marginVertical: SIZES.h2 }}>
-              <Text style={{ ...FONTS.bodyNormalBold }}>{title}</Text>
-              <InputText
-                placeholder={placeholder}
-                name="bid_price"
-                style={{ marginTop: 4 }}
-                onChangeText={handleChange('bid_price')}
-                onBlur={handleBlur('bid_price')}
-                error={touched.bid_price && errors.bid_price}
-                value={values.bid_price}
-                type="number-pad"
+              <CustomButton
+                onPress={handleSubmit}
+                buttonStyle={{ width: '100%' }}
+                title="Kirim"
+                enabled={isValid && !errors.bid_price}
               />
-              {touched.bid_price && errors.bid_price && (
-                <HelperText text={t(errors.bid_price)} />
-              )}
             </View>
-            <CustomButton
-              onPress={handleSubmit}
-              buttonStyle={{ width: '100%' }}
-              title={status ? 'Menunggu respon penjual' : 'Kirim'}
-              enabled={isValid && !errors.bid_price && status !== 'pending'}
-            />
-          </View>
+          </ScrollView>
         )}
       </Formik>
     </BottomSheet>
